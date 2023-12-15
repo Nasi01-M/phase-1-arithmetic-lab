fec0ff33bed0aa7aa19428ac98ee80c3e047d44f
MICHELLE NASIRISI <wanjikunasirisi1@gmail.com>
2023-12-15 13:14:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+// Multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+// Random
+let random = Math.floor(Math.random() * 10) + 1;
+
+// Modulo
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+
+// Max
+let max = Math.max(1, 2, 3, 4, 5, 20);
+
+module.exports = { multiply, random, mod, max };
